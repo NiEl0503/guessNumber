eaa431259c3999e4daba2c4420a17946dac6e433
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2024-03-02 20:29:05 -0300

    obtaining the entry (number) of the player

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ import random
 
 secret_number = random.randint(1, 100)
 print("generated random number:", secret_number)
+
+#input function to get the player input
+while True:
+        #player's turn
+        attempt_player = int(input("Your turn. Make your guess: "))
+      
