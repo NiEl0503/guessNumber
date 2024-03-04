8be935dc8613f900c4fd002986361108e3d3b44a
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2024-03-04 20:36:31 -0300

    loop created to compare the player's guess with the secret number

--- main.py
+++ main.py
@@ -9,3 +9,10 @@ while True:
         #player's turn
         attempt_player = int(input("Your turn. Make your guess: "))
       
+        if attempt_player < secret_number:
+            print("Your assumption has been low 🙁")
+        elif attempt_player > secret_number:
+            print("Your assumption has been high 🤯")
+        else:
+            print("🚀 Congratulations! you guessed the secret number 🚀")
+            break
