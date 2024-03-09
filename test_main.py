449729b2e6f90ef35115ff4abdfcb5b719cdc981
ncontreraskanan@gmail.com <125376144+NiEl0503@users.noreply.github.com>
2024-03-09 17:06:52 -0300

    TestPlayerGuess

--- test_main.py
+++ test_main.py
@@ -9,5 +9,17 @@ class TestGetSecretNumber(unittest.TestCase):
         secret_number = main.get_secret_number()
         self.assertEqual(secret_number, 42)
 
+class TestPlayerGuess(unittest.TestCase):
+
+    @patch('builtins.input', side_effect=['10'])
+    def test_player_guess_valid_input(self, mock_input):
+        guess = main.player_guess('Test')
+        self.assertEqual(guess, 10)
+
+    @patch('builtins.input', side_effect=['abc', '10'])
+    def test_player_guess_invalid_input(self, mock_input):
+        guess = main.player_guess('Test')
+        self.assertEqual(guess, 10)
+
 if __name__ == '__main__':
     unittest.main()
